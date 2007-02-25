a2134c3fa5eb440a11fc3eab2b1c938a3069eaa9
lotodore <pokerth@lotharmay.de>
2007-02-25 19:56:58 +0000

    Additional include files for linux.

--- src/net/genericsocket.h
+++ src/net/genericsocket.h
@@ -27,6 +27,9 @@
 #else
 #include <sys/types.h>
 #include <sys/socket.h>
+#include <sys/select.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <netdb.h>
