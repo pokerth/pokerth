df2e7a84dd83c5e68aafdeb2315b487dc0c58f51
lotodore <pokerth@lotharmay.de>
2007-06-02 20:02:55 +0000

    Do not exit if sound cannot be initialized.

--- src/sound/sdlplayer.cpp
+++ src/sound/sdlplayer.cpp
@@ -29,19 +29,15 @@ SDLPlayer::~SDLPlayer()
 void SDLPlayer::initAudio() {
 
 	audio_rate = 44100;
-  	audio_format = AUDIO_S16; /* 16-bit stereo */
-  	audio_channels = 2;
-  	audio_buffers = 4096;
+	audio_format = AUDIO_S16; /* 16-bit stereo */
+	audio_channels = 2;
+	audio_buffers = 4096;
 	music = NULL;
 
 	SDL_Init(SDL_INIT_AUDIO);
 
-	if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers)) {
-		printf("Unable to open audio!\n");
-		exit(1);
-	}
-	
-	Mix_QuerySpec(&audio_rate, &audio_format, &audio_channels);
+	if(Mix_OpenAudio(audio_rate, audio_format, audio_channels, audio_buffers) == 0)
+		Mix_QuerySpec(&audio_rate, &audio_format, &audio_channels);
 }
 
 void SDLPlayer::playSound(string audioString) {
@@ -52,7 +48,7 @@ void SDLPlayer::playSound(string audioString) {
 	/* Actually loads up the music */
 	string completeAudioString = audioString + ".wav";
 	music = Mix_LoadMUS(completeAudioString.c_str());
-	
+
 	/* This begins playing the music - the first argument is a
 	pointer to Mix_Music structure, and the second is how many
 	times you want it to loop (use -1 for infinite, and 0 to
@@ -70,15 +66,15 @@ void SDLPlayer::playSound(string audioString) {
 void SDLPlayer::audioDone() {
 
 	Mix_HaltMusic();
-  	Mix_FreeMusic(music);
-  	music = NULL;
+	Mix_FreeMusic(music);
+	music = NULL;
 }
 
 void SDLPlayer::closeAudio() {
 
 	Mix_HaltMusic();
 	Mix_FreeMusic(music);
-  	music = NULL;
+	music = NULL;
 	Mix_CloseAudio();
-  	SDL_Quit();
+	SDL_Quit();
 }
