0470bceeff4d84e0e9603921b809ceb1fe25bf79
lotodore <pokerth@lotharmay.de>
2007-06-11 21:51:58 +0000

    Make sounds work in network games.

--- src/net/common/clientstate.cpp
+++ src/net/common/clientstate.cpp
@@ -657,9 +657,10 @@ ClientStateRunHand::InternalProcess(ClientThread &client, boost::shared_ptr<NetP
 			// Refresh GUI
 			if (tmpPlayer->getMyID() == 0)
 				client.GetGui().disableMyButtons();
+			else
+				client.GetGui().refreshAction(tmpPlayer->getMyID(), tmpPlayer->getMyAction());
 			client.GetGui().refreshPot();
 			client.GetGui().refreshSet();
-			client.GetGui().refreshAction();
 			client.GetGui().refreshCash();
 			client.GetGui().refreshButton();
 		}
