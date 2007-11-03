8e4901719c4d422a4102f9b4f5662a973f32948c
lotodore <pokerth@lotharmay.de>
2007-11-03 21:05:20 +0000

    Fixed setting last players turn

--- src/net/common/clientstate.cpp
+++ src/net/common/clientstate.cpp
@@ -991,7 +991,6 @@ ClientStateRunHand::InternalProcess(ClientThread &client, boost::shared_ptr<NetP
 			// Next player's turn.
 			curGame->getCurrentHand()->getCurrentBeRo()->setCurrentPlayersTurnId(tmpPlayer->getMyID());
 			curGame->getCurrentHand()->getCurrentBeRo()->setMinimumRaise(turnData.minimumRaise);
-			curGame->getCurrentHand()->getCurrentBeRo()->setPlayersTurn(tmpPlayer->getMyID());
 
 			// Mark current player in GUI.
 			int guiStatus = 2;
