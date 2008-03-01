1ba22682ee432248104bc0d0662892e86bef7a97
doitux <f.hammer@web.de>
2008-03-01 21:31:50 +0000

    adds opengametimeoutmsgbox (non functional)

---
+++ src/gui/qt/gamelobbydialog/opengametimeoutmsgbox/opengametimeoutmsgboximpl.cpp
@@ -0,0 +1,24 @@
+//
+// C++ Implementation: opengametimeoutmsgboximpl
+//
+// Description: 
+//
+//
+// Author: Felix Hammer <f.hammer@web.de>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#include "opengametimeoutmsgboximpl.h"
+
+openGameTimeoutMsgBoxImpl::openGameTimeoutMsgBoxImpl()
+ : QMessageBox()
+{
+}
+
+
+openGameTimeoutMsgBoxImpl::~openGameTimeoutMsgBoxImpl()
+{
+}
+
+
---
+++ src/gui/qt/gamelobbydialog/opengametimeoutmsgbox/opengametimeoutmsgboximpl.h
@@ -0,0 +1,29 @@
+//
+// C++ Interface: opengametimeoutmsgboximpl
+//
+// Description: 
+//
+//
+// Author: Felix Hammer <f.hammer@web.de>, (C) 2008
+//
+// Copyright: See COPYING file that comes with this distribution
+//
+//
+#ifndef OPENGAMETIMEOUTMSGBOXIMPL_H
+#define OPENGAMETIMEOUTMSGBOXIMPL_H
+
+#include <QMessageBox>
+
+/**
+	@author Felix Hammer <f.hammer@web.de>
+*/
+class openGameTimeoutMsgBoxImpl : public QMessageBox
+{
+public:
+    openGameTimeoutMsgBoxImpl();
+
+    ~openGameTimeoutMsgBoxImpl();
+
+};
+
+#endif
