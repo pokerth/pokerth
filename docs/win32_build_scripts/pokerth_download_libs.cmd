411ede3500bdf41506b749e28e501741287cebcd
lotodore <pokerth@lotharmay.de>
2009-05-04 23:14:07 +0000

    Updated windows build script, now using: - tdm mingw gcc 4.4 - Qt 4.5.1 - boost 1.39 + other minor updates. Update of build tool still pending. The script will be changed to retrieve a stable revision instead of HEAD.

--- docs/win32_build_scripts/pokerth_download_libs.cmd
+++ docs/win32_build_scripts/pokerth_download_libs.cmd
@@ -51,17 +51,17 @@ echo The process will start if you press any other key.
 pause
 echo.
 echo Downloading mingw packages
-%PKTH_OldDir%\third_party_apps\curl -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/tdm-gcc/gcc-4.3.3-tdm-1-dw2-core.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/tdm-gcc/gcc-4.3.3-tdm-1-dw2-g++.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/binutils-2.19.1-mingw32-bin.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingwrt-3.15.2-mingw32-dev.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingwrt-3.15.2-mingw32-dll.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/w32api-3.11.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingw32-make-3.81-20080326-3.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingw-utils-0.3.tar.gz
+%PKTH_OldDir%\third_party_apps\curl -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/tdm-gcc/gcc-4.4.0-tdm-1-dw2-core-2.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/tdm-gcc/gcc-4.4.0-tdm-1-dw2-g++-2.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/binutils-2.19.1-mingw32-bin.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingwrt-3.15.2-mingw32-dev.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingwrt-3.15.2-mingw32-dll.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/w32api-3.13-mingw32-dev.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingw32-make-3.81-20080326-3.tar.gz -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/mingw/mingw-utils-0.3.tar.gz
 if not errorlevel 0 goto downloadFailed
 echo.
 echo Unpacking mingw packages
 mkdir mingw
-%PKTH_OldDir%\third_party_apps\7za x -y gcc-4.3.3-tdm-1-dw2-core.tar.gz
-%PKTH_OldDir%\third_party_apps\7za x -y -omingw gcc-4.3.3-tdm-1-dw2-core.tar
-del gcc-4.3.3-tdm-1-core.tar
-%PKTH_OldDir%\third_party_apps\7za x -y gcc-4.3.3-tdm-1-dw2-g++.tar.gz
-%PKTH_OldDir%\third_party_apps\7za x -y -omingw gcc-4.3.3-tdm-1-dw2-g++.tar
-del gcc-4.3.3-tdm-1-g++.tar
+%PKTH_OldDir%\third_party_apps\7za x -y gcc-4.4.0-tdm-1-dw2-core-2.tar.gz
+%PKTH_OldDir%\third_party_apps\7za x -y -omingw gcc-4.4.0-tdm-1-dw2-core-2.tar
+del gcc-4.4.0-tdm-1-dw2-core-2.tar
+%PKTH_OldDir%\third_party_apps\7za x -y gcc-4.4.0-tdm-1-dw2-g++-2.tar.gz
+%PKTH_OldDir%\third_party_apps\7za x -y -omingw gcc-4.4.0-tdm-1-dw2-g++-2.tar
+del gcc-4.4.0-tdm-1-dw2-g++-2.tar
 %PKTH_OldDir%\third_party_apps\7za x -y binutils-2.19.1-mingw32-bin.tar.gz
 %PKTH_OldDir%\third_party_apps\7za x -y -omingw binutils-2.19.1-mingw32-bin.tar
 del binutils-2.19.1-mingw32-bin.tar
@@ -71,9 +71,9 @@ del mingwrt-3.15.2-mingw32-dev.tar
 %PKTH_OldDir%\third_party_apps\7za x -y mingwrt-3.15.2-mingw32-dll.tar.gz
 %PKTH_OldDir%\third_party_apps\7za x -y -omingw mingwrt-3.15.2-mingw32-dll.tar
 del mingwrt-3.15.2-mingw32-dll.tar
-%PKTH_OldDir%\third_party_apps\7za x -y w32api-3.11.tar.gz
-%PKTH_OldDir%\third_party_apps\7za x -y -omingw w32api-3.11.tar
-del w32api-3.11.tar
+%PKTH_OldDir%\third_party_apps\7za x -y w32api-3.13-mingw32-dev.tar.gz
+%PKTH_OldDir%\third_party_apps\7za x -y -omingw w32api-3.13-mingw32-dev.tar
+del w32api-3.13-mingw32-dev.tar
 %PKTH_OldDir%\third_party_apps\7za x -y mingw32-make-3.81-20080326-3.tar.gz
 %PKTH_OldDir%\third_party_apps\7za x -y -omingw mingw32-make-3.81-20080326-3.tar
 del mingw32-make-3.81-20080326-3.tar
@@ -91,7 +91,7 @@ ren gcc-dw2.exe gcc.exe
 ren gcov-dw2.exe gcov.exe
 ren mingw32-c++-dw2.exe mingw32-c++.exe
 ren mingw32-g++-dw2.exe mingw32-g++.exe
-ren mingw32-gcc-4.3.3-dw2.exe mingw32-gcc-4.3.3.exe
+ren mingw32-gcc-4.4.0-dw2.exe mingw32-gcc-4.4.0.exe
 ren mingw32-gcc-dw2.exe mingw32-gcc.exe
 cd /d %PKTH_BaseDir%
 echo.
@@ -107,20 +107,14 @@ cd /d %PKTH_BaseDir%
 if not exist %PKTH_BaseDir%\qt. (
 echo.
 echo Downloading Qt
-%PKTH_OldDir%\third_party_apps\curl -O ftp://ftp.ntua.gr/pub/X11/Qt/qt/source/qt-win-opensource-src-4.4.3.zip
+%PKTH_OldDir%\third_party_apps\curl -O http://get.qtsoftware.com/qt/source/qt-win-opensource-src-4.5.1.zip
 if not errorlevel 0 goto downloadFailed
 echo.
 echo Unpacking Qt
-%PKTH_OldDir%\third_party_apps\7za x -y qt-win-opensource-src-4.4.3.zip
+%PKTH_OldDir%\third_party_apps\7za x -y qt-win-opensource-src-4.5.1.zip
 REM Wait 5 seconds for the file cache, else ren might fail.
 @ping 127.0.0.1 -n 5 -w 1000 > nul
-ren qt-win-opensource-src-4.4.3 qt
-echo.
-echo Patching Qt
-cd qt
-copy %PKTH_OldDir%\helper_src\qt.patch .
-%PKTH_OldDir%\third_party_apps\pat_ch -p1 -i qt.patch
-cd /d %PKTH_BaseDir%
+ren qt-win-opensource-src-4.5.1 qt
 )
 if not exist %PKTH_BaseDir%\qt goto qtFailure
 echo.
@@ -163,9 +157,9 @@ cd /d %PKTH_BaseDir%
 if not exist %PKTH_BaseDir%\GnuTLS. (
 echo.
 echo Downloading GnuTLS
-%PKTH_OldDir%\third_party_apps\curl -O http://josefsson.org/gnutls4win/gnutls-2.6.4.zip
+%PKTH_OldDir%\third_party_apps\curl -O http://josefsson.org/gnutls4win/gnutls-2.6.6.zip
 mkdir GnuTLS
-%PKTH_OldDir%\third_party_apps\7za x -y -oGnuTLS gnutls-2.6.4.zip
+%PKTH_OldDir%\third_party_apps\7za x -y -oGnuTLS gnutls-2.6.6.zip
 REM Wait 5 seconds for the file cache.
 @ping 127.0.0.1 -n 5 -w 1000 > nul
 REM Remove files for dynamic linking, we do not want to accidently use them
@@ -177,16 +171,16 @@ cd /d %PKTH_BaseDir%
 if not exist %PKTH_BaseDir%\curl. (
 echo.
 echo Downloading curl
-%PKTH_OldDir%\third_party_apps\curl -O http://curl.haxx.se/download/curl-7.19.3.tar.bz2
+%PKTH_OldDir%\third_party_apps\curl -O http://curl.haxx.se/download/curl-7.19.4.tar.bz2
 if not errorlevel 0 goto downloadFailed
 echo.
 echo Unpacking curl
-%PKTH_OldDir%\third_party_apps\7za x -y curl-7.19.3.tar.bz2
-%PKTH_OldDir%\third_party_apps\7za x -y curl-7.19.3.tar
-del curl-7.19.3.tar
+%PKTH_OldDir%\third_party_apps\7za x -y curl-7.19.4.tar.bz2
+%PKTH_OldDir%\third_party_apps\7za x -y curl-7.19.4.tar
+del curl-7.19.4.tar
 REM Wait 5 seconds for the file cache, else ren might fail.
 @ping 127.0.0.1 -n 5 -w 1000 > nul
-ren curl-7.19.3 curl
+ren curl-7.19.4 curl
 )
 echo.
 echo Compiling curl
@@ -198,18 +192,18 @@ cd /d %PKTH_BaseDir%
 if not exist %PKTH_BaseDir%\boost. (
 echo.
 echo Downloading boost and bjam
-%PKTH_OldDir%\third_party_apps\curl -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/boost/boost_1_38_0.7z -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/boost/boost-jam-3.1.17-1-ntx86.zip
+%PKTH_OldDir%\third_party_apps\curl -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/boost/boost_1_39_0.7z -O http://%SOURCEFORGE_MIRROR%.sourceforge.net/sourceforge/boost/boost-jam-3.1.17-1-ntx86.zip
 if not errorlevel 0 goto downloadFailed
 echo.
 echo Unpacking boost and bjam
-%PKTH_OldDir%\third_party_apps\7za x -y boost_1_38_0.7z
+%PKTH_OldDir%\third_party_apps\7za x -y boost_1_39_0.7z
 %PKTH_OldDir%\third_party_apps\7za x -y boost-jam-3.1.17-1-ntx86.zip
 REM Wait 5 seconds for the file cache, else ren might fail.
 @ping 127.0.0.1 -n 5 -w 1000 > nul
 move boost-jam-3.1.17-1-ntx86\bjam.exe mingw\bin\bjam.exe
 move boost-jam-3.1.17-1-ntx86\LICENSE_1_0.txt mingw\bin\bjam_LICENSE_1_0.txt
 rd boost-jam-3.1.17-1-ntx86
-ren boost_1_38_0 boost
+ren boost_1_39_0 boost
 )
 echo.
 echo Compiling boost
@@ -257,14 +251,14 @@ cd /d %PKTH_BaseDir%
 if not exist %PKTH_BaseDir%\svn. (
 echo.
 echo Downloading subversion
-%PKTH_OldDir%\third_party_apps\curl -O http://subversion.tigris.org/files/documents/15/44589/svn-win32-1.5.5.zip
+%PKTH_OldDir%\third_party_apps\curl -O http://subversion.tigris.org/files/documents/15/45600/svn-win32-1.6.1.zip
 if not errorlevel 0 goto downloadFailed
 echo.
 echo Unpacking subversion
-%PKTH_OldDir%\third_party_apps\7za x -y svn-win32-1.5.5.zip
+%PKTH_OldDir%\third_party_apps\7za x -y svn-win32-1.6.1.zip
 REM Wait 5 seconds for the file cache, else ren might fail.
 @ping 127.0.0.1 -n 5 -w 1000 > nul
-ren svn-win32-1.5.5 svn
+ren svn-win32-1.6.1 svn
 )
 if not exist %PKTH_BaseDir%\svn\bin\svn.exe goto svnFailure
 if not exist %PKTH_BaseDir%\pokerth. (
