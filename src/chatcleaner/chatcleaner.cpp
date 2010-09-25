918bd478cd65b60954b8748c461fef5f44c5b0c5
lotodore <pokerth@lotharmay.de>
2010-09-25 20:03:36 +0000

    Start chatcleaner as daemon.

--- src/chatcleaner/chatcleaner.cpp
+++ src/chatcleaner/chatcleaner.cpp
@@ -1,9 +1,21 @@
 #include <QtCore/QCoreApplication>
 #include "cleanerserver.h"
 
+#ifdef _WIN32
+	#include <process.h>
+#else
+	#include <unistd.h>
+	#ifndef daemon
+		int daemon(int, int);
+	#endif
+#endif
+
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
 	CleanerServer server;
+#ifndef _WIN32
+	daemon(0, 0);
+#endif
     return a.exec();
 }
