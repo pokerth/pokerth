6464cf40d71dcdf69bb703732640f6710db10b57
lotodore <pokerth@lotharmay.de>
2010-10-11 20:20:17 +0000

    Testing build bot (again).

--- src/net/common/serverlobbybot.cpp
+++ src/net/common/serverlobbybot.cpp
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2009 by Lothar May                                      *
+ *   Copyright (C) 2009-2010 by Lothar May                                 *
  *                                                                         *
  *   This program is free software; you can redistribute it and/or modify  *
  *   it under the terms of the GNU General Public License as published by  *
