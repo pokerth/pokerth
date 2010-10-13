320a545972a7c1b933e06eef56b0d7db9089b60a
lotodore <pokerth@lotharmay.de>
2010-10-13 21:37:25 +0000

    Adding cmd script to prepare windows build with buildbot.

--- win_prepare_build.cmd
+++ win_prepare_build.cmd
@@ -7,18 +7,11 @@ echo.
 
 SET PKTH_OldDir=%CD%
 SET PKTH_BaseDir=%1
-if exist %PKTH_BaseDir%\mingw64\bin\mingw32-make.exe goto mingw64
 SET PATH=%PKTH_BaseDir%\qt\bin;%PKTH_BaseDir%\mingw32\bin;%PATH%
-SET PKTH_Conf=""
-goto postmingw
-:mingw64
-SET PATH=%PKTH_BaseDir%\qt\bin;%PKTH_BaseDir%\mingw64\bin;%PATH%
-SET PKTH_Conf=pkth_win64
-:postmingw
 SET QMAKESPEC=win32-g++
 SET QTDIR=%PKTH_BaseDir%\qt
 
 if exist Makefile. (
 mingw32-make distclean
 )
-qmake "CONFIG+=release %PKTH_Conf%" DEFINES+=BOOST_USE_WINDOWS_H pokerth.pro
+qmake DEFINES+=BOOST_USE_WINDOWS_H pokerth.pro
