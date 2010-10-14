708f8f9e0b677e38aabbf6f85768990158f6f76b
lotodore <pokerth@lotharmay.de>
2010-10-14 08:03:08 +0000

    Instead of preparing the build, we build directly to prevent path problems.

--- win_prepare_build.cmd
+++ win_prepare_build.cmd
@@ -5,13 +5,12 @@ echo BECAUSE THE PROGRAM IS LICENSED FREE OF CHARGE, THERE IS NO WARRANTY
 echo FOR THE PROGRAM, TO THE EXTENT PERMITTED BY APPLICABLE LAW.
 echo.
 
-SET PKTH_OldDir=%CD%
 SET PKTH_BaseDir=%1
 SET PATH=%PKTH_BaseDir%\qt\bin;%PKTH_BaseDir%\mingw32\bin;%PATH%
 SET QMAKESPEC=win32-g++
 SET QTDIR=%PKTH_BaseDir%\qt
 
-if exist Makefile. (
-mingw32-make distclean
-)
 qmake DEFINES+=BOOST_USE_WINDOWS_H pokerth.pro
+mingw32-make release
+REM Add automatic packaging or copying here.
+mingw32-make distclean
