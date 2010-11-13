c583d190022555d77cb8804422ca183b0d4da049
lotodore <pokerth@lotharmay.de>
2010-11-13 01:43:23 +0000

    Adding test to let a game run until the end.

--- tests/src/pokerth_test/RunNormalGameTest.java
+++ tests/src/pokerth_test/RunNormalGameTest.java
@@ -1,47 +1,34 @@
-/*  PokerTH automated tests.
-	Copyright (C) 2010 Lothar May
-
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
-
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
-
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
 package pokerth_test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.fail;
 
 import java.util.ArrayList;
 import java.util.Collection;
 
 import org.junit.Test;
 
-import pokerth_protocol.*;
+import pokerth_protocol.NetGameInfo;
+import pokerth_protocol.NonZeroId;
+import pokerth_protocol.PokerTHMessage;
+import pokerth_protocol.StartEventMessage;
 import pokerth_protocol.NetGameInfo.EndRaiseModeEnumType;
 import pokerth_protocol.NetGameInfo.NetGameTypeEnumType;
 import pokerth_protocol.StartEventMessage.StartEventMessageSequenceType;
 
-public class StartNormalGameTest extends TestBase {
+
+public class RunNormalGameTest extends TestBase {
 
 	@Test
-	public void testGameStartMessage() throws Exception {
+	public void testRunNormalGameAsGuest() throws Exception {
 		guestInit();
 
 		Collection<Integer> l = new ArrayList<Integer>();
-		NetGameInfo gameInfo = createGameInfo(10, EndRaiseModeEnumType.EnumType.doubleBlinds, 0, 100, GuestUser + " start normal game", l, 10, 0, 11, 20000);
+		NetGameInfo gameInfo = createGameInfo(5, EndRaiseModeEnumType.EnumType.doubleBlinds, 0, 100, GuestUser + " run normal game", l, 10, 0, 2, 2000);
 		sendMessage(createJoinGameRequestMsg(
 				gameInfo,
 				NetGameTypeEnumType.EnumType.normalGame,
 				10,
-				7,
+				5,
 				""));
 
 		PokerTHMessage msg;
@@ -77,6 +64,29 @@ public class StartNormalGameTest extends TestBase {
 			failOnErrorMessage(msg);
 			fail("Invalid message.");
 		}
-	}
 
+		do {
+			msg = receiveMessage();
+		} while (
+				msg.isGameListMessageSelected()
+				|| msg.isGamePlayerMessageSelected()
+				|| msg.isStartEventMessageSelected()
+				);
+
+		do {
+			msg = receiveMessage();
+		} while (
+				msg.isHandStartMessageSelected()
+				|| msg.isDealFlopCardsMessageSelected()
+				|| msg.isDealRiverCardMessageSelected()
+				|| msg.isDealTurnCardMessageSelected()
+				|| msg.isPlayersTurnMessageSelected()
+				|| msg.isPlayersActionDoneMessageSelected()
+				|| msg.isEndOfHandMessageSelected()
+				|| msg.isAllInShowCardsMessageSelected()
+				);
+		if (!msg.isEndOfGameMessageSelected()) {
+			fail("No end of game received.");
+		}
+	}
 }
