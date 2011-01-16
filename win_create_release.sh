2d05ccbafebc37a283f7dfdbc0c0317e96297ab4
lotodore <pokerth@lotharmay.de>
2011-01-16 15:39:47 +0000

    Copy pokerth.exe file.

--- win_create_release.sh
+++ win_create_release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir ./PokerTH
-mv release/pokerth.exe ./PokerTH/
+cp release/pokerth.exe ./PokerTH/
 cp -R ./data ./PokerTH/
 find ./PokerTH/ -name ".svn" | xargs rm -Rf 
 zip -r PokerTH-windows.zip ./PokerTH
