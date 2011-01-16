b96a3d288e1d418114ac8cb005efa018d3448f8c
lotodore <pokerth@lotharmay.de>
2011-01-16 15:25:23 +0000

    Fixing removing svn directories.

--- win_create_release.sh
+++ win_create_release.sh
@@ -2,5 +2,5 @@
 mkdir ./PokerTH
 mv release/pokerth.exe ./PokerTH/
 cp -R ./data ./PokerTH/
-find ./PokerTH/ -name ".svn" -exec rm -rf {} \;
+find ./PokerTH/ -name ".svn" | xargs rm -Rf 
 zip -r PokerTH-windows.zip ./PokerTH
