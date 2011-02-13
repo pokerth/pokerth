71cc99c6efad48559e1422ca76a51e246ad5632b
lotodore <pokerth@lotharmay.de>
2011-02-13 21:45:04 +0000

    Fix warning.

--- tests/src/pokerth_test/GameListTest.java
+++ tests/src/pokerth_test/GameListTest.java
@@ -2,7 +2,6 @@ package pokerth_test;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 import java.net.Socket;
 import java.util.ArrayList;
