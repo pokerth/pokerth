bc2c31acf0f992976372af17b9599f5c801273df
lotodore <pokerth@lotharmay.de>
2011-06-12 20:32:51 +0000

    Updating Linux build helper.

--- docs/linux_build_scripts/prepare_pokerth.sh
+++ docs/linux_build_scripts/prepare_pokerth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 PKTH_GSASL="libgsasl-1.4.4"
 PKTH_GSASL_FILE="$PKTH_GSASL.tar.gz"
-PKTH_QT="qt-everywhere-opensource-src-4.7.1"
+PKTH_QT="qt-everywhere-opensource-src-4.7.3"
 PKTH_QT_FILE="$PKTH_QT.tar.gz"
-PKTH_BOOST="boost_1_46_0"
+PKTH_BOOST="boost_1_46_1"
 PKTH_BOOST_FILE="$PKTH_BOOST.tar.bz2"
 
 if [ ! -e $PKTH_GSASL_FILE ]; then
