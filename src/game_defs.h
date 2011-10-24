c867a789d3d7264c4e43481026c21d6f8c29712b
floty <f.thauer@web.de>
2011-10-24 20:53:32 +0000

    - bugfix new entry in PlayerActionLog

--- src/game_defs.h
+++ src/game_defs.h
@@ -69,7 +69,7 @@ enum PlayerAction {
 	PLAYER_ACTION_CALL,
 	PLAYER_ACTION_BET,
 	PLAYER_ACTION_RAISE,
-	PLAYER_ACTION_ALLIN
+        PLAYER_ACTION_ALLIN
 };
 
 enum PlayerActionCode {
@@ -80,20 +80,21 @@ enum PlayerActionCode {
 };
 
 enum PlayerActionLog {
-	LOG_ACTION_DEALER = 0,
-	LOG_ACTION_SMALL_BLIND,
-	LOG_ACTION_BIG_BLIND,
-	LOG_ACTION_FOLD,
-	LOG_ACTION_CHECK,
-	LOG_ACTION_CALL,
-	LOG_ACTION_BET,
-	LOG_ACTION_ALL_IN,
-	LOG_ACTION_SHOW,
-	LOG_ACTION_HAS,
-	LOG_ACTION_WIN,
-	LOG_ACTION_WIN_SIDE_POT,
-	LOG_ACTION_SIT_OUT,
-	LOG_ACTION_WIN_GAME,
+        LOG_ACTION_NONE = 0,
+        LOG_ACTION_DEALER,      // starts as dealer
+        LOG_ACTION_SMALL_BLIND, // posts small blind
+        LOG_ACTION_BIG_BLIND,   // posts big blind
+        LOG_ACTION_FOLD,        // folds
+        LOG_ACTION_CHECK,       // checks
+        LOG_ACTION_CALL,        // calls
+        LOG_ACTION_BET,         // bets
+        LOG_ACTION_ALL_IN,      // is all in with
+        LOG_ACTION_SHOW,        // shows
+        LOG_ACTION_HAS,         // has
+        LOG_ACTION_WIN,         // wins
+        LOG_ACTION_WIN_SIDE_POT,// wins (side pot)
+        LOG_ACTION_SIT_OUT,     // sits out
+        LOG_ACTION_WIN_GAME     // wins game
 };
 
 enum DenyKickPlayerReason {
