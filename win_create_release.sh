1d25d8b545a321b6acb54aa24cbddca410bbb468
lotodore <pokerth@lotharmay.de>
2011-10-29 20:06:04 +0000

    Changing Windows release script.

--- win_create_release.sh
+++ win_create_release.sh
@@ -1,6 +1,24 @@
 #!/bin/bash
-mkdir ./PokerTH
-cp release/pokerth.exe ./PokerTH/
-cp -R ./data ./PokerTH/
-find ./PokerTH/ -name ".svn" | xargs rm -Rf 
-zip -r PokerTH-windows.zip ./PokerTH
+if [[ -n "$1" && -n "$2" ]] ; then
+
+        rm -r ./PokerTH-$1*
+        mkdir ./PokerTH-$1
+        cp release/pokerth.exe ./PokerTH-$1/
+        cp release/pokerth.exe .
+        cp -R ./data ./PokerTH-$1/
+        find ./PokerTH-$1/ -name ".svn" | xargs rm -Rf
+        zip -r PokerTH-$1-windows.zip ./PokerTH-$1
+
+        ~/bitrock-installbuilder/installbuilder-7.2.5/bin/builder build ~/slave/pokerth-release-win32/build/pokerth_bitrock_windows.xml windows
+
+        mv ~/bitrock-installbuilder/installbuilder-7.2.5/output/PokerTH-$1-windows-installer.exe .
+        scp PokerTH-$1-windows-installer.exe PokerTH-$1-windows.zip lotodore,pokerth@frs.sourceforge.net:/home/frs/project/p/po/pokerth/PokerTH/$2/
+        md5sum PokerTH-$1-windows-installer.exe
+        rm PokerTH-$1-windows-installer.exe
+        md5sum PokerTH-$1-windows.zip
+        rm PokerTH-$1-windows.zip
+        rm -r ./PokerTH-$1*
+        rm pokerth.exe
+else
+echo "Please use 'win-create-release.sh version sf-dir-version'"
+fi
