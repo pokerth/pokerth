aa230ec17b6eec197fc9e950a9982ad4b4edbb24
lotodore <pokerth@lotharmay.de>
2012-01-22 18:27:58 +0000

    Reverting last change. This should be fixed in libircclient.

--- src/net/common/ircthread.cpp
+++ src/net/common/ircthread.cpp
@@ -20,7 +20,6 @@
 #include <net/ircthread.h>
 #include <net/socket_msg.h>
 #include <libircclient/libircclient.h>
-#include <libircclient/libirc_rfcnumeric.h>
 #include <boost/algorithm/string/predicate.hpp>
 #include <queue>
 #include <sstream>
