b893f98d0106759a2771542b628aed050351c45c
doitux <f.hammer@web.de>
2012-02-04 21:37:52 +0000

    make cppcheck happy ;)

--- src/gui/qt/gamelobbydialog/mynicklistsortfilterproxymodel.cpp
+++ src/gui/qt/gamelobbydialog/mynicklistsortfilterproxymodel.cpp
@@ -20,7 +20,7 @@
 #include <QtCore>
 
 MyNickListSortFilterProxyModel::MyNickListSortFilterProxyModel(QObject *parent)
-	: QSortFilterProxyModel(parent), filterState(0)
+: QSortFilterProxyModel(parent), filterState(0), lastFilterStateAlpha(true), lastFilterStateCountry(false)
 {
 }
 
