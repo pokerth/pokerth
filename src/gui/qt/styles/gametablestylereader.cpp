e8a24210630bdeef6211e495e6f0e396d91ffda6
doitux <f.hammer@web.de>
2012-05-29 15:15:29 +0200

    set and cash label changes for mobile gui

--- src/gui/qt/styles/gametablestylereader.cpp
+++ src/gui/qt/styles/gametablestylereader.cpp
@@ -69,8 +69,8 @@ GameTableStyleReader::GameTableStyleReader(ConfigFile *c, QWidget *w)
     tabBarPaddingTop = "0";
     tabBarPaddingSide = "9";
     textBrowserFontsize= "10";
-    cashFontSize = "12";
-    setLabelFontSize = "12";
+    cashFontSize = "13";
+    setLabelFontSize = "13";
     playerNameLabelFontSize = "11";
     smallBoardFontSize = "13";
     bigBoardFontSize = "17";
