9deeec96c45b69bee13bd9533871928282bd7abf
lotodore <pokerth@lotharmay.de>
2013-08-31 19:50:40 +0200

    Mac OS script changes for Qt5.

--- mac_post_make.sh
+++ mac_post_make.sh
@@ -9,8 +9,8 @@
 # of binary-size if you leave Qt out.
 # (see http://trolltech.com/developer/downloads/qt/mac)
 
-QT_FW_PATH="/Library/Frameworks"
-QT_PLUGIN_PATH="/Developer/Applications/Qt/plugins"
+QT_FW_PATH="/Developer/Qt5/5/clang_64/lib"
+QT_PLUGIN_PATH="/Developer/Qt5/5/clang_64/plugins"
 SDL_FW_PATH="/Library/Frameworks"
 APPLICATION="./pokerth.app"
 BINARY="$APPLICATION/Contents/MacOs/pokerth"
@@ -44,37 +44,33 @@ if [ "$1" != "--without-qt" ] ; then
         cp -R $QT_FW_PATH/QtNetwork.framework $BINARY_FW_PATH
         # remove debug versions
         rm -f $APPLICATION/Contents/Frameworks/QtCore.framework/QtCore_debug
-        rm -rf $APPLICATION/Contents/Frameworks/QtCore.framework/QtCore_debug.dSYM
         rm -f $APPLICATION/Contents/Frameworks/QtCore.framework/QtCore_debug.prl
-        rm -f $APPLICATION/Contents/Frameworks/QtCore.framework/Versions/4/QtCore_debug
+        rm -f $APPLICATION/Contents/Frameworks/QtCore.framework/Versions/5/QtCore_debug
         rm -f $APPLICATION/Contents/Frameworks/QtGui.framework/QtGui_debug
-        rm -rf $APPLICATION/Contents/Frameworks/QtGui.framework/QtGui_debug.dSYM
         rm -f $APPLICATION/Contents/Frameworks/QtGui.framework/QtGui_debug.prl
-        rm -f $APPLICATION/Contents/Frameworks/QtGui.framework/Versions/4/QtGui_debug
+        rm -f $APPLICATION/Contents/Frameworks/QtGui.framework/Versions/5/QtGui_debug
         rm -f $APPLICATION/Contents/Frameworks/QtSql.framework/QtSql_debug
-        rm -rf $APPLICATION/Contents/Frameworks/QtSql.framework/QtSql_debug.dSYM
         rm -f $APPLICATION/Contents/Frameworks/QtSql.framework/QtSql_debug.prl
-        rm -f $APPLICATION/Contents/Frameworks/QtSql.framework/Versions/4/QtSql_debug
+        rm -f $APPLICATION/Contents/Frameworks/QtSql.framework/Versions/5/QtSql_debug
         rm -f $APPLICATION/Contents/Frameworks/QtNetwork.framework/QtNetwork_debug
-        rm -rf $APPLICATION/Contents/Frameworks/QtNetwork.framework/QtNetwork_debug.dSYM
         rm -f $APPLICATION/Contents/Frameworks/QtNetwork.framework/QtNetwork_debug.prl
-        rm -f $APPLICATION/Contents/Frameworks/QtNetwork.framework/Versions/4/QtNetwork_debug
+        rm -f $APPLICATION/Contents/Frameworks/QtNetwork.framework/Versions/5/QtNetwork_debug
 
 	# redirect binary to use integrated frameworks
-        QTCORE="QtCore.framework/Versions/4.0/QtCore"
-        QTGUI="QtGui.framework/Versions/4.0/QtGui"
-        QTSQL="QtSql.framework/Versions/4.0/QtSql"
-        QTNETWORK="QtNetwork.framework/Versions/4.0/QtNetwork"
+        QTCORE="QtCore.framework/Versions/5/QtCore"
+        QTGUI="QtGui.framework/Versions/5/QtGui"
+        QTSQL="QtSql.framework/Versions/5/QtSql"
+        QTNETWORK="QtNetwork.framework/Versions/5/QtNetwork"
         install_name_tool -id @executable_path/../Frameworks/$QTCORE $BINARY_FW_PATH/$QTCORE
-	install_name_tool -id @executable_path/../Frameworks/$QTGUI $BINARY_FW_PATH/$QTGUI
+        install_name_tool -id @executable_path/../Frameworks/$QTGUI $BINARY_FW_PATH/$QTGUI
         install_name_tool -id @executable_path/../Frameworks/$QTSQL $BINARY_FW_PATH/$QTSQL
         install_name_tool -id @executable_path/../Frameworks/$QTSQL $BINARY_FW_PATH/$QTNETWORK
         QTCORE_LINK=$(otool -L $BINARY | grep QtCore | cut -d"(" -f1 | cut -f2)
-	QTGUI_LINK=$(otool -L $BINARY | grep QtGui | cut -d"(" -f1 | cut -f2)
+        QTGUI_LINK=$(otool -L $BINARY | grep QtGui | cut -d"(" -f1 | cut -f2)
         QTSQL_LINK=$(otool -L $BINARY | grep QtSql | cut -d"(" -f1 | cut -f2)
         QTNETWORK_LINK=$(otool -L $BINARY | grep QtNetwork | cut -d"(" -f1 | cut -f2)
         install_name_tool -change $QTCORE_LINK @executable_path/../Frameworks/$QTCORE $BINARY
-	install_name_tool -change $QTGUI_LINK @executable_path/../Frameworks/$QTGUI $BINARY
+        install_name_tool -change $QTGUI_LINK @executable_path/../Frameworks/$QTGUI $BINARY
         install_name_tool -change $QTSQL_LINK @executable_path/../Frameworks/$QTSQL $BINARY
         install_name_tool -change $QTNETWORK_LINK @executable_path/../Frameworks/$QTNETWORK $BINARY
         install_name_tool -change $QTCORE_LINK @executable_path/../Frameworks/$QTCORE $BINARY_PLUGIN_PATH/imageformats/libqgif.dylib
