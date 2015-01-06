bace7cd9875a3e11cfb25d948958de7af36f237b
lotodore <pokerth@lotharmay.de>
2015-01-06 15:43:24 +0100

    Workaround for issue #282 (should be backward compatible).

--- src/net/common/ircthread.cpp
+++ src/net/common/ircthread.cpp
@@ -32,14 +32,14 @@
 #include <net/socket_helper.h>
 #include <net/ircthread.h>
 #include <net/socket_msg.h>
-#include <libircclient/libircclient.h>
+#include <libircclient.h>
 
 // We need to do the following to handle different versions of libircclient.
 // Sadly, libircclient doesn't have actual definitions for its versions in its headers.
 // However, we can use a definition that appeared in the same version we need
 // to check for. Hacky, but hey, it works.
 #ifdef LIBIRC_OPTION_SSL_NO_VERIFY
-#include <libircclient/libirc_rfcnumeric.h>
+#include <libirc_rfcnumeric.h>
 #endif
 
 #include <boost/algorithm/string/predicate.hpp>
