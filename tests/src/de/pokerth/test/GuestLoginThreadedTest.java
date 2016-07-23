159fd1e0946f578300fc1ecf796b117973f27432
Albert Medela <albert.medela@gmail.com>
2016-07-23 21:00:13 +0200

    JUnit test for several guest users adapted -- correction

---
+++ tests/src/de/pokerth/test/GuestLoginThreadedTest.java
@@ -0,0 +1,11 @@
+package de.pokerth.test;
+
+import org.junit.Test;
+
+public class GuestLoginThreadedTest extends TestBase {
+
+	@Test
+	public void testInitThreadedMessage() throws Exception {
+		guestInitThreaded();
+	}
+}
