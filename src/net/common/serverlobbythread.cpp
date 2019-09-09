53602107d9370380526385a1f45a556bda9dac34
Kai Philipp <kphilipp@inquies.de>
2019-09-09 20:42:31 +0200

    ip-check off

--- src/net/common/serverlobbythread.cpp
+++ src/net/common/serverlobbythread.cpp
@@ -1429,10 +1429,11 @@ ServerLobbyThread::HandleNetPacketJoinGame(boost::shared_ptr<SessionData> sessio
 				SendJoinGameFailed(session, joinGame.gameid(), NTF_NET_JOIN_INVALID_PASSWORD);
 			} else if (tmpData.gameType == GAME_TYPE_RANKING) {
 				if(!joinGame.spectateonly()
-					   && session->GetClientAddr() != SERVER_ADDRESS_LOCALHOST_STR
-					   && session->GetClientAddr() != SERVER_ADDRESS_LOCALHOST_STR_V4V6
-					   && session->GetClientAddr() != SERVER_ADDRESS_LOCALHOST_STR_V4
-					   && game->IsClientAddressConnected(session->GetClientAddr())) {
+					//    && session->GetClientAddr() != SERVER_ADDRESS_LOCALHOST_STR
+					//    && session->GetClientAddr() != SERVER_ADDRESS_LOCALHOST_STR_V4V6
+					//    && session->GetClientAddr() != SERVER_ADDRESS_LOCALHOST_STR_V4
+					//    && game->IsClientAddressConnected(session->GetClientAddr())) {
+				){
 					SendJoinGameFailed(session, joinGame.gameid(), NTF_NET_JOIN_IP_BLOCKED);
 				}else if (!session->IsPlayerAllowedToJoinLimitRank()) {
 					SendJoinGameFailed(session, joinGame.gameid(), NTF_NET_JOIN_IP_BLOCKED);
