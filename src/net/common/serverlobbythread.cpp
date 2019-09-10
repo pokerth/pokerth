1eb486660eaafec906e3708b7b962b977620b764
Kai Philipp <kphilipp@inquies.de>
2019-09-10 21:01:02 +0200

    bugfix

--- src/net/common/serverlobbythread.cpp
+++ src/net/common/serverlobbythread.cpp
@@ -1428,7 +1428,7 @@ ServerLobbyThread::HandleNetPacketJoinGame(boost::shared_ptr<SessionData> sessio
 				SendJoinGameFailed(session, joinGame.gameid(), NTF_NET_JOIN_NOT_INVITED);
 			} else if (!game->CheckPassword(password)) {
 				SendJoinGameFailed(session, joinGame.gameid(), NTF_NET_JOIN_INVALID_PASSWORD);
-			} else if (tmpData.gameType == GAME_TYPE_RANKING && !session->IsPlayerAllowedToJoinLimitRank()) {
+			} else if (tmpData.gameType == GAME_TYPE_RANKING && !session->GetPlayerData()->IsPlayerAllowedToJoinLimitRank()) {
 				SendJoinGameFailed(session, joinGame.gameid(), NTF_NET_JOIN_GUEST_FORBIDDEN);
 			// } else if (tmpData.gameType == GAME_TYPE_RANKING && !joinGame.spectateonly()
 			// 	   && session->GetClientAddr() != SERVER_ADDRESS_LOCALHOST_STR
