cfced4e4805ec1c549c8dcbaf9f52d7596a2cfe2
Kai Philipp <kphilipp@inquies.de>
2019-09-13 18:36:13 +0200

    bugfix

--- src/net/common/serverlobbythread.cpp
+++ src/net/common/serverlobbythread.cpp
@@ -1815,7 +1815,8 @@ LOG_ERROR("last_games from db = " << dbPlayerData.last_games);
 		std::vector<long> last_games;
 		std::stringstream ss(dbPlayerData.last_games);
 		for (string i; ss >> i;) {
-			last_games.push_back(stol(i));    
+			if(i.length() > 0)
+				last_games.push_back(stol(i));    
 			if (ss.peek() == ',')
 				ss.ignore();
 		}
