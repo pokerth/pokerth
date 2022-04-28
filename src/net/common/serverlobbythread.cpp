c33888e1e613f11210c631f927a64ee425a3dc9c
Kai Philipp <kphilipp@inquies.de>
2022-04-28 19:53:21 +0200

    Huckleberry global notice

--- src/net/common/serverlobbythread.cpp
+++ src/net/common/serverlobbythread.cpp
@@ -1505,7 +1505,7 @@ ServerLobbyThread::HandleNetPacketChatRequest(boost::shared_ptr<SessionData> ses
 				// Only allow private messages to players which are not in running games.
 				boost::shared_ptr<ServerGame> tmpGame = targetSession->GetGame();
 				if (!tmpGame || !tmpGame->IsRunning()) {
-          if(targetSession->GetPlayerData()->GetDBId() == 338 /* bbcbot */ && (session->GetPlayerData()->GetDBId() == 338 /* bbcbot */ || session->GetPlayerData()->GetDBId() == 36 /* sp0ck */ || session->GetPlayerData()->GetDBId() == 37 /* boehmi */|| session->GetPlayerData()->GetDBId() == 45 /* RankingKing */|| session->GetPlayerData()->GetDBId() == 73 /* q4z1 */) && chatRequest.chattext().substr (0, 3) == "gn ")
+          if(targetSession->GetPlayerData()->GetDBId() == 338 /* bbcbot */ && (session->GetPlayerData()->GetDBId() == 338 /* bbcbot */ || session->GetPlayerData()->GetDBId() == 36 /* sp0ck */ || session->GetPlayerData()->GetDBId() == 37 /* boehmi */|| session->GetPlayerData()->GetDBId() == 45 /* RankingKing */|| session->GetPlayerData()->GetDBId() == 73 /* q4z1 */|| session->GetPlayerData()->GetDBId() == 82 /* Huckleberry */) && chatRequest.chattext().substr (0, 3) == "gn ")
           {
             // bbcbot pm from admins - global notice => /msg bbcbot gn This is a global Notice
             LOG_ERROR("Global Notice: " << chatRequest.chattext().substr(3) << " von player_id " << session->GetPlayerData()->GetDBId());
